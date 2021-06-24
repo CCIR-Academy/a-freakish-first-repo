c647dd09cd78171b7e6d143b34273335e41b97a7
a-feaskish <ashoktimsina147181@gmail.com>
2021-06-24 22:02:49 +0545

    bot.py commit

--- bot.py
+++ bot.py
@@ -1,14 +1,56 @@
-from chai_py import ChaiBot
+from chai_py import ChaiBot, Update
+import datetime 
 
-class mathbot(ChaiBot):
-    def setup(self, date_created, creator, nickname):
+class AnyBot(ChaiBot):
+    def setup(self):
         self.logger.info("Hi there!")
-        self.date_created = date_created
-        self.creator = creator
-        self.nickname = nickname
+        name = input('Enter the name of your bot:')
+        human = input('Who is this human?:')
+        self.name = name
+        self.human = human
 
-    def print_all_data(self):
-        print(self.date_created, self.creator, self.nickname)
+    async def on_message(self, update: Update) -> str:
+         return f"Echo: {update.latest_message.text}"
 
-bot = mathbot(1, aasu, hell)
+    #def ask_for_number(self):
 
+
+from chai_py import TRoom
+
+t_room = TRoom([AnyBot()])
+t_room.chat()
+
+
+
+# """ from chai_py import ChaiBot, Update, TRoom
+# import datetime, asyncio
+# import nest_asyncio
+# nest_asyncio.apply()
+
+# # Important Note: Don't worry about all the asyncio thing as they are just tweaks to allow such bot to be run on Colab. You won't need them if you run your bot locally.
+
+# from chai_py import ChaiBot, Update, TRoom
+
+# class EchoBot(ChaiBot):
+#     def setup(self):
+#         self.logger.info("Hello, world!")
+
+#     async def on_message(self, update: Update) -> str:
+#         return f"Echo: {update.latest_message.text}"
+
+# t_room = TRoom([EchoBot()])
+# t_room.chat() """
+
+# from chai_py import ChaiBot, Update
+
+# class Bot(ChaiBot):
+#     def setup(self):
+#         self.logger.info("Hello, world!")
+
+#     async def on_message(self, update: Update) -> str:
+#         return "Hi, I'm ExampleBot."
+
+# from chai_py import TRoom
+
+# t_room = TRoom([Bot()])
+# t_room.chat()
