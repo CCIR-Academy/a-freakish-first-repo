4af3d2dd58e7f376c28045659b8c453a72ecb9aa
a-feaskish <ashoktimsina147181@gmail.com>
2021-07-02 22:27:55 +0545

    phase three 1st commit

---
+++ unknown.py
@@ -0,0 +1,28 @@
+# inst = 'Enter the no of lines you want to print:'
+# no = int(input(inst)) #no of rows
+# no_s = no-1 #no of spaces in front
+# for i in range(1, no+1):
+#     print(" " * no_s, '* '*i)
+#     no_s = no_s - 1
+
+# inst = 'Enter the no of lines you want to print:'
+# no = int(input(inst)) #no of rows
+# no_s = 0 #no of spaces in front
+# for i in reversed(range(1, no+1)):
+#     print(" " * no_s, '* '*i)
+#     no_s = no_s + 1
+
+# people = ['a', 'b', 'c']
+# for p in people:
+#     print(p)
+
+inst = 'Enter the no of lines you want to print:'
+no = int(input(inst)) #no of rows
+no_s = no-1 #no of spaces in front
+for i in range(1, no+1):
+    print(" " * no_s, '*'*i)
+    no_s = no_s - 1
+    
+
+
+
