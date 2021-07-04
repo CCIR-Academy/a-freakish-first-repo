4c35f7bf78961177a707f7493ffbae01a38f4fda
a-feaskish <ashoktimsina147181@gmail.com>
2021-07-05 00:33:51 +0545

    section 3 final commit

--- task3.3.py
+++ task3.3.py
@@ -24,4 +24,45 @@ while False:
     cursor.close()
 
 
+        
+connection = sqlite3.connect('sample.db')
+cursor = connection.cursor()
+
+#task 1 : giving the price form the time of the transaction
+while False:
+    date = 1624711283094
+    rows = cursor.execute(
+        "SELECT price FROM btcdat2 WHERE time = ?",
+        (date,),
+    ).fetchall()
+    print(rows)
+
+# task 2 : List all datasets, which have a quantity of greater than 1
+while False:
+    quantity = 1
+    rows = cursor.execute(
+        "SELECT price, quantity ,time FROM btcdat2 WHERE quantity > ?",
+        (quantity,),
+        ).fetchall()
+    print(rows)
+
+#task 3 : Using COUNT, estimate the number of datasets where the quantiy was lower than 0.1
+
+# cutoff = 0.1
+# rows = cursor.execute("SELECT price FROM btcdat2 WHERE quantity < ?",
+#     (cutoff,),).fetchall()
+# print(len(rows))
+while False:
+    cutoff = 0.1
+    rows = cursor.execute("SELECT COUNT(price) FROM btcdat2 WHERE quantity < ?",
+        (cutoff,),).fetchall()
+    print(rows)
+
+# task 4 : Using AVG, calculate the average price of all datasets.
+
+avg = cursor.execute("SELECT AVG(price) from btcdat2").fetchall()
+print(avg)
+ 
+
+
 
